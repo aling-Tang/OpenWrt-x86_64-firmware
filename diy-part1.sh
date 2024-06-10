22da9f24c70615bf183254aee84cea63b3f647d4
aling-Tang <116485841+aling-Tang@users.noreply.github.com>
2024-06-10 20:36:11 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,5 +16,3 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
-echo 'src-git hwfuture https://github.com/hongweifuture/openwrt-packages' >>feeds.conf.default
-echo 'src-git pwdep https://github.com/hongweifuture/pwdep.git' >>feeds.conf.default
